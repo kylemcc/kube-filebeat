1bab308d1e3ce1548bce4a5c76045ce9db438a83
Kyle McCullough <kylemcc@gmail.com>
2016-08-29 12:41:45 -0500

    start filebeat with foreman so it gets restarted; use an empty filebeat config initially so filebeat doesn't error out

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 kubeproxy: kubectl --server="$KUBERNETES_API_URL" proxy
 kubegen: /app/start-kube-gen.sh
+filebeat: filebeat -e -c /app/filebeat.yml
--- start-kube-gen.sh
+++ start-kube-gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 1
-exec kube-gen -watch -type pods -wait 2s:10s -post-cmd '/app/restart-filebeat.sh' /app/filebeat.yml.tmpl /app/filebeat.yml
+exec kube-gen -watch -type pods -wait 2s:10s -post-cmd 'pkill filebeat' /app/filebeat.yml.tmpl /app/filebeat.yml
