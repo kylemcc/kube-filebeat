645b1fed892f2053b1aeb531edd3e3b73aef6c95
Kyle McCullough <kylemcc@gmail.com>
2016-08-29 18:25:48 -0500

    fix kill command

--- kill-filebeat.sh
+++ kill-filebeat.sh
@@ -2,7 +2,7 @@
 
 cnt=0
 pid=$(pgrep -x filebeat)
-while kill -o $pid > /dev/null 2>&1
+while kill -0 $pid > /dev/null 2>&1
 do
 	if [ $cnt -gt "0" ]; then
 		sleep $((2**$cnt))
@@ -10,10 +10,10 @@ do
 	
 	if [ $cnt -lt "3" ]; then
 		echo "Killing filebeat pid:$pid"
-		kill $pid
+		kill $pid > /dev/null 2>&1
 	else
 		echo "Force-killing filebeat pid:$pid"
-		kill -9 $pid
+		kill -9 $pid > /dev/null 2>&1
 	fi
 	cnt=$((cnt+1))
 done
