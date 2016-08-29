a846beb79cadb25a82cf1f0707d2bff15d54a94a
Kyle McCullough <kylemcc@gmail.com>
2016-08-29 17:35:39 -0500

    only kill filebeat once

--- kill-filebeat.sh
+++ kill-filebeat.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 cnt=0
-while pgrep filebeat > /dev/null 2>&1
+pid=$(pgrep -x filebeat)
+while kill -o $pid > /dev/null 2>&1
 do
 	if [ $cnt -gt "0" ]; then
-		sleep 1
+		sleep $((2**$cnt))
 	fi
 	
 	if [ $cnt -lt "3" ]; then
-		kill $(pgrep filebeat)
+		echo "Killing filebeat pid:$pid"
+		kill $pid
 	else
-		kill -9 $(pgrep filebeat)
+		echo "Force-killing filebeat pid:$pid"
+		kill -9 $pid
 	fi
 	cnt=$((cnt+1))
 done
